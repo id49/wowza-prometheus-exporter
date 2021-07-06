1196a0b818ee4205a719fd340ee2cb62e47c26ea
Tulio Faria <tuliofaria@gmail.com>
2021-07-06 19:43:57 -0300

    initial version

---
+++ index.js
@@ -0,0 +1,163 @@
+require('dotenv').config()
+
+const express = require('express')
+const axios = require('axios')
+const Prometheus = require('prom-client')
+
+const auth = {
+  username: process.env.WOWZA_USER,
+  password: process.env.WOWZA_PASS
+}
+
+const app = express()
+app.get('/', async (req, res) => {
+  if (req.query.a === process.env.AUTH) {
+    res.end(await Prometheus.register.metrics())
+  } else {
+    res.send({ auth: false })
+  }
+})
+/*
+const collectDefaultMetrics = Prometheus.collectDefaultMetrics
+collectDefaultMetrics({
+  timeout: 5000
+})*/
+
+const serviceHealthStreamsGauge = new Prometheus.Gauge({
+  name: 'streams_health',
+  help: 'Health streams',
+  labelNames: ['server']
+})
+const serviceTotalStreamsGauge = new Prometheus.Gauge({
+  name: 'streams_total',
+  help: 'Total streams',
+  labelNames: ['server']
+})
+const serverUptimeGauge = new Prometheus.Gauge({
+  name: 'uptime',
+  help: 'Server uptime',
+  labelNames: ['server']
+})
+const serverBytesInGauge = new Prometheus.Gauge({
+  name: 'server_bytesIn',
+  help: 'Server bytesIn',
+  labelNames: ['server']
+})
+const serverBytesInRateGauge = new Prometheus.Gauge({
+  name: 'server_bytesInRate',
+  help: 'Server bytesInRate',
+  labelNames: ['server']
+})
+const serverBytesOutGauge = new Prometheus.Gauge({
+  name: 'server_bytesOut',
+  help: 'Server bytesOut',
+  labelNames: ['server']
+})
+const serverBytesOutRateGauge = new Prometheus.Gauge({
+  name: 'server_bytesOutRate',
+  help: 'Server bytesOutRate',
+  labelNames: ['server']
+})
+const serverTotalConnectionsGauge = new Prometheus.Gauge({
+  name: 'server_totalConnections',
+  help: 'Server totalConnections',
+  labelNames: ['server']
+})
+
+const collectServerStatus = async () => {
+  const url = `http://${process.env.WOWZA_SERVER}:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/live/monitoring/current`
+  const res = await axios({
+    method: 'get',
+    url,
+    auth
+  })
+  const {
+    uptime,
+    bytesIn,
+    bytesOut,
+    bytesInRate,
+    bytesOutRate,
+    totalConnections
+  } = res.data
+
+  serverUptimeGauge.set(
+    {
+      server: 'robin'
+    },
+    uptime
+  )
+  serverBytesInGauge.set(
+    {
+      server: 'robin'
+    },
+    bytesIn
+  )
+  serverBytesInRateGauge.set(
+    {
+      server: 'robin'
+    },
+    bytesInRate
+  )
+  serverBytesOutGauge.set(
+    {
+      server: 'robin'
+    },
+    bytesOut
+  )
+  serverBytesOutRateGauge.set(
+    {
+      server: 'robin'
+    },
+    bytesOutRate
+  )
+
+  serverTotalConnectionsGauge.set(
+    {
+      server: 'robin'
+    },
+    totalConnections
+  )
+}
+const collectStreamsStatus = async () => {
+  const url = `http://${process.env.WOWZA_SERVER}:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/live/instances/_definst_`
+  const res = await axios({
+    method: 'get',
+    url,
+    auth
+  })
+  const { incomingStreams } = res.data
+  const validStreams = incomingStreams.filter(
+    (item) => item.sourceIp !== item.name
+  )
+  const health = validStreams.filter((item) => item.isConnected)
+  serviceHealthStreamsGauge.set(
+    {
+      server: 'robin'
+    },
+    health.length
+  )
+  serviceTotalStreamsGauge.set(
+    {
+      server: 'robin'
+    },
+    validStreams.length
+  )
+}
+
+setInterval(() => {
+  try {
+    collectStreamsStatus()
+  } catch (err) {}
+  try {
+    collectServerStatus()
+  } catch (err) {}
+}, 10000)
+
+const port = process.env.PORT || 3000
+app.listen(port, (err) => {
+  if (err) {
+    console.log('problem running exporter', err)
+  } else {
+    console.log('exporter running on port', port)
+  }
+})
